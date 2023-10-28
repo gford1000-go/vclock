e2583d2b08e8ab7b858ba38aa13c559e23cf3e70
Geoff Ford <geoff.golang.12@gmail.com>
2023-10-28 13:23:56 +0100

    Added more benchmark tests

--- bench_test.go
+++ bench_test.go
@@ -1,8 +1,15 @@
 package vclock
 
-import "testing"
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"testing"
 
-func BenchmarkNew(b *testing.B) {
+	"golang.org/x/exp/rand"
+)
+
+func BenchmarkNewWithDefer(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		c, _ := New(Clock{"a": 0}, nil)
@@ -15,6 +22,7 @@ func BenchmarkTick(b *testing.B) {
 	c, _ := New(Clock{"a": 0}, nil)
 	defer c.Close()
 
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		c.Tick("a")
 	}
@@ -29,6 +37,7 @@ func BenchmarkTickWithHistory(b *testing.B) {
 	c, _ := NewWithHistory(Clock{"a": 0}, nil)
 	defer c.Close()
 
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		c.Tick("a")
 	}
@@ -43,6 +52,26 @@ func BenchmarkMerge(b *testing.B) {
 	other, _ := New(Clock{"b": 1}, nil)
 	defer other.Close()
 
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		c, _ := New(Clock{"a": 0}, nil)
+		defer c.Close()
+
+		c.Merge(other)
+	}
+}
+
+func BenchmarkMergeLarge(b *testing.B) {
+
+	c := Clock{}
+	for i := 0; i < 1024; i++ {
+		c[fmt.Sprint(i)] = rand.Uint64()
+	}
+
+	other, _ := New(c, nil)
+	defer other.Close()
+
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		c, _ := New(Clock{"a": 0}, nil)
 		defer c.Close()
@@ -56,6 +85,7 @@ func BenchmarkMergeWithHistory(b *testing.B) {
 	other, _ := NewWithHistory(Clock{"b": 1}, nil)
 	defer other.Close()
 
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		c, _ := New(Clock{"a": 0}, nil)
 		defer c.Close()
@@ -69,8 +99,25 @@ func BenchmarkBytes(b *testing.B) {
 	c, _ := New(Clock{"a": 1}, nil)
 	defer c.Close()
 
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		c.Bytes()
+	}
+}
+
+func BenchmarkBytesLarge(b *testing.B) {
+
+	c := Clock{}
+	for i := 0; i < 1024; i++ {
+		c[fmt.Sprint(i)] = rand.Uint64()
+	}
+
+	vc, _ := New(c, nil)
+	defer vc.Close()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		vc.Bytes()
 	}
 }
 
@@ -79,6 +126,7 @@ func BenchmarkBytesWithHistory(b *testing.B) {
 	c, _ := NewWithHistory(Clock{"a": 1}, nil)
 	defer c.Close()
 
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		c.Bytes()
 	}
@@ -91,10 +139,48 @@ func BenchmarkFromBytes(b *testing.B) {
 
 	buf, _ := c.Bytes()
 
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		n, _ := FromBytes(buf, nil)
 		n.Close()
 	}
+}
+
+func BenchmarkPrepGOBDecoder(b *testing.B) {
+
+	c, _ := New(Clock{"a": 1}, nil)
+	defer c.Close()
+
+	buf, _ := c.Bytes()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+
+		b := new(bytes.Buffer)
+		b.Write(buf)
+		gob.NewDecoder(b)
+	}
+
+}
+
+func BenchmarkDeserialiseClock(b *testing.B) {
+
+	c, _ := New(Clock{"a": 1}, nil)
+	defer c.Close()
+
+	buf, _ := c.Bytes()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+
+		b := new(bytes.Buffer)
+		b.Write(buf)
+		dec := gob.NewDecoder(b)
+
+		m := Clock{}
+		dec.Decode(&m)
+	}
+
 }
 
 func BenchmarkFromBytesWithHistory(b *testing.B) {
@@ -104,8 +190,16 @@ func BenchmarkFromBytesWithHistory(b *testing.B) {
 
 	buf, _ := c.Bytes()
 
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		n, _ := FromBytes(buf, nil)
 		n.Close()
+	}
+}
+
+func BenchmarkNewOnly(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		New(Clock{"a": 0}, nil)
 	}
 }
