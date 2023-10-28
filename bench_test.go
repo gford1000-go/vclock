1940d95e23761ed7a2fc8c1b04b78182fada9673
Geoff Ford <geoff.golang.12@gmail.com>
2023-10-28 12:50:36 +0100

    Added benchmark tests

---
+++ bench_test.go
@@ -0,0 +1,87 @@
+package vclock
+
+import "testing"
+
+func BenchmarkTick(b *testing.B) {
+
+	c, _ := New(Clock{"a": 0}, nil)
+
+	for i := 0; i < b.N; i++ {
+		c.Tick("a")
+	}
+
+	if v, ok := c.Get("a"); !ok || v != uint64(b.N) {
+		b.Fatalf("Clock has wrong value: expected %v, got %v\n", b.N, v)
+	}
+}
+
+func BenchmarkTickWithHistory(b *testing.B) {
+
+	c, _ := NewWithHistory(Clock{"a": 0}, nil)
+
+	for i := 0; i < b.N; i++ {
+		c.Tick("a")
+	}
+
+	if v, ok := c.Get("a"); !ok || v != uint64(b.N) {
+		b.Fatalf("Clock has wrong value: expected %v, got %v\n", b.N, v)
+	}
+}
+
+func BenchmarkMerge(b *testing.B) {
+
+	other, _ := New(Clock{"b": 1}, nil)
+
+	for i := 0; i < b.N; i++ {
+		c, _ := New(Clock{"a": 0}, nil)
+		c.Merge(other)
+	}
+}
+
+func BenchmarkMergeWithHistory(b *testing.B) {
+
+	other, _ := NewWithHistory(Clock{"b": 1}, nil)
+
+	for i := 0; i < b.N; i++ {
+		c, _ := New(Clock{"a": 0}, nil)
+		c.Merge(other)
+	}
+}
+
+func BenchmarkBytes(b *testing.B) {
+
+	c, _ := New(Clock{"a": 1}, nil)
+
+	for i := 0; i < b.N; i++ {
+		c.Bytes()
+	}
+}
+
+func BenchmarkBytesWithHistory(b *testing.B) {
+
+	c, _ := NewWithHistory(Clock{"a": 1}, nil)
+
+	for i := 0; i < b.N; i++ {
+		c.Bytes()
+	}
+}
+
+func BenchmarkFromBytes(b *testing.B) {
+
+	c, _ := New(Clock{"a": 1}, nil)
+	buf, _ := c.Bytes()
+
+	for i := 0; i < b.N; i++ {
+		FromBytes(buf, nil)
+	}
+}
+
+func BenchmarkFromBytesWithHistory(b *testing.B) {
+
+	c, _ := NewWithHistory(Clock{"a": 1}, nil)
+	buf, _ := c.Bytes()
+
+	for i := 0; i < b.N; i++ {
+		FromBytes(buf, nil)
+	}
+}
