47906661da985e5299d32cf582cde383564e54d0
Geoff Ford <geoff.golang.12@gmail.com>
2023-11-05 09:57:42 +0000

    Upgraded syncmap version, allows InMemoryShortener serialisation

--- shortener.go
+++ shortener.go
@@ -1,17 +1,24 @@
 package vclock
 
 import (
+	"bytes"
+	"encoding/gob"
 	"errors"
 
 	"github.com/gford1000-go/syncmap"
 )
 
+// ShortenedMap is the underlying type expected to be serialised by IdentifierShortener implementations
+type ShortenedMap map[string]string
+
 // IdentifierShortener provides functions to shorten vector clock
 // identifiers to minimise the overall memory footprint of the clock.
 type IdentifierShortener interface {
-	Name() string
-	Shorten(s string) string
-	Recover(s string) string
+	Name() string            // Name of the shortener - msut be unique
+	Shorten(s string) string // Returns the shortened version of the supplied string
+	Recover(s string) string // Recovers the original string from the shortened version
+	Bytes() ([]byte, error)  // The full map of shortened strings to original strings as a serialised ShortenedMap
+	Merge(b []byte) error    // Merge the contents of the ShortenedMap into the instance
 }
 
 // Shortener is the function that applies the transformation
@@ -19,6 +26,7 @@ type Shortener func(string) string
 
 var errShortenerIsNil = errors.New("shortener must not be nil")
 var errShortenerNameIsNil = errors.New("shortener name must be non-empty string")
+var errSerialiseNameMismatch = errors.New("shortener name mismatch - deserialisation not possible")
 
 // NewInMemoryShortener creates an instance of InMemoryShortener that will
 // use the specified Shortener
@@ -62,4 +70,50 @@ func (h *InMemoryShortener) Recover(s string) string {
 	} else {
 		return ss
 	}
+}
+
+type serial struct {
+	N string
+	B []byte
+}
+
+func (h *InMemoryShortener) Bytes() ([]byte, error) {
+	b, err := h.sm.Bytes()
+	if err != nil {
+		return nil, err
+	}
+
+	buf := new(bytes.Buffer)
+	enc := gob.NewEncoder(buf)
+
+	s := serial{
+		N: h.Name(),
+		B: b,
+	}
+
+	if err := enc.Encode(&s); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func (h *InMemoryShortener) Merge(b []byte) error {
+
+	buf := new(bytes.Buffer)
+	buf.Write(b)
+	dec := gob.NewDecoder(buf)
+
+	s := serial{
+		B: []byte{},
+	}
+
+	if err := dec.Decode(&s); err != nil {
+		return err
+	}
+
+	if s.N != h.Name() {
+		return errSerialiseNameMismatch
+	}
+
+	return h.sm.Merge(s.B)
 }
